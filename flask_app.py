0540413ece8a7e65fbe7ab80777262c4a5ce0e6b
unknown <jergra@yahoo.com>
2023-01-09 16:26:28 -0800

    fix c to add correct url

--- flask_app.py
+++ flask_app.py
@@ -113,7 +113,7 @@ def fetch_potd(cur_date):
     print("------------------------------")
     print("THE_DATA from aaaa to b:")
     print(c)
-    # c = c.replace('href="', 'href="https://en.wikipedia.org')
+    c = c.replace('href="', 'href="https://en.wikipedia.org')
     print("------------------------------")
     print("THE_DATA from aaaa to b with working links:")
     # print(c)
